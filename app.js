7c2e7a095aa23154c0c51e6d999cce5e67f1ca7e
Jason Wells <flipstock@gmail.com>
2015-12-23 17:36:44 -0700

    status and check endpoints

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var zxcvbn = require('zxcvbn');
+
+// Status message
+app.get('/', function (req, res) {
+  res.send({ status: 'up' });
+});
+
+// Check password strength with zxcvbn
+app.get('/check', function (req, res) {
+  if (req.query.password) {
+    res.json(zxcvbn(req.query.password));
+  }
+  else {
+    res.status(400).json({ error: 'No password provided' });
+  }
+}); 
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('pwd-js listening at http:://%s:%s', host, port);
+});
